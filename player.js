357ab2fb691db5c3d7844a5242712ca802141b5b
zahrevans <526zevans@gmail.com>
2025-05-19 09:44:27 -0400

    gen 1 gyms

--- player.js
+++ player.js
@@ -1,142 +1,84 @@
-const players = [
+const player = [
   {
-    firstName: 'Francisco',
-    lastName: 'Lindor',
-    position: 'SS',
-    age: 31,
-    photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/32129.png&w=350&h=254'
+    "name": "Brock",
+    "gen": 1,
+    "gym": 1,
+    "city": "Pewter City",
+    "type": "Rock",
+    "badge": "Boulder",
+    "sprite": "https://play.pokemonshowdown.com/sprites/trainers/brock.png",
+    "Ace": "Onix"
   },
   {
-    firstName: 'Pete',
-    lastName: 'Alonso',
-    position: '1B',
-    age: 30,
-    photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/37498.png&w=350&h=254'
+    "name": "Misty",
+    "gen": 1,
+    "gym": 2,
+    "city": "Cerulean City",
+    "type": "Water",
+    "badge": "Cascade",
+    "sprite": "https://play.pokemonshowdown.com/sprites/trainers/misty.png",
+    "Ace": "Starmie"
   },
   {
-    firstName: 'Brandon',
-    lastName: 'Nimmo',
-    position: 'CF',
-    age: 32,
-    photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/32159.png&w=350&h=254'
+    "name": "Lt.Surge",
+    "gen": 1,
+    "gym": 3,
+    "city": "Vermilion City",
+    "type": "Electric",
+    "badge": "Thunder",
+    "sprite": "https://play.pokemonshowdown.com/sprites/trainers/ltsurge.png",
+    "Ace": "Raichu"
   },
   {
-    firstName: 'Starling',
-    lastName: 'Marte',
-    position: 'RF',
-    age: 36,
-    photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/31127.png'
+    "name": "Erika",
+    "gen": 1,
+    "gym": 4,
+    "city": "Celadon City",
+    "type": "Grass",
+    "badge": "Rainbow",
+    "sprite": "https://play.pokemonshowdown.com/sprites/trainers/erika.png",
+    "Ace": "Vileplume"
   },
   {
-    firstName: 'Jeff',
-    lastName: 'McNeil',
-    position: '2B',
-    age: 33,
-    photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/36062.png'
+    "name": "Koga",
+    "gen": 1,
+    "gym": 5,
+    "city": "Fuchsia City",
+    "type": "Poison",
+    "badge": "Soul",
+    "sprite": "https://play.pokemonshowdown.com/sprites/trainers/koga.png",
+    "Ace": "Weezing"
   },
   {
-    firstName: 'Francisco',
-    lastName: 'Álvarez',
-    position: 'C',
-    age: 23,
-    photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916460.png'
+    "name": "Sabrina",
+    "gen": 1,
+    "gym": 6,
+    "city": "Saffron City",
+    "type": "Psychic",
+    "badge": "Marsh",
+    "sprite": "https://play.pokemonshowdown.com/sprites/trainers/sabrina.png",
+    "Ace": "Alakazam"
   },
   {
-    firstName: 'Brett',
-    lastName: 'Baty',
-    position: '3B',
-    age: 25,
-    photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/42414.png&w=350&h=254'
+    "name": "Blaine",
+    "gen": 1,
+    "gym": 7,
+    "city": "Cinnabar Island",
+    "type": "Fire",
+    "badge": "Volcano",
+    "sprite": "https://play.pokemonshowdown.com/sprites/trainers/blaine.png",
+    "Ace": "Arcanine"
   },
   {
-    firstName: 'Luisangel',
-    lastName: 'Acuña',
-    position: 'INF',
-    age: 23,
-    photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916462.png'
+    "name": "Giovanni",
+    "gen": 1,
+    "gym": 8,
+    "city": "Viridian City",
+    "type": "Ground",
+    "badge": "Earth",
+    "sprite": "https://play.pokemonshowdown.com/sprites/trainers/giovanni.png",
+    "Ace": "Rhydon"
   },
-  {
-    firstName: 'Mark',
-    lastName: 'Vientos',
-    position: 'DH',
-    age: 25,
-    photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916463.png'
-  },
-  {
-    firstName: 'Tyrone',
-    lastName: 'Taylor',
-    position: 'OF',
-    age: 31,
-    photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/34886.png'
-  },
-  {
-    firstName: 'José',
-    lastName: 'Azocar',
-    position: 'OF',
-    age: 28,
-    photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916464.png'
-  },
-  {
-    firstName: 'Juan',
-    lastName: 'Soto',
-    position: 'OF',
-    age: 26,
-    photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/40921.png'
-  },
-  {
-    firstName: 'Kodai',
-    lastName: 'Senga',
-    position: 'SP',
-    age: 32,
-    photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916465.png'
-  },
-  {
-    firstName: 'David',
-    lastName: 'Peterson',
-    position: 'SP',
-    age: 29,
-    photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916466.png'
-  },
-  {
-    firstName: 'Griffin',
-    lastName: 'Canning',
-    position: 'SP',
-    age: 29,
-    photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916467.png'
-  },
-  {
-    firstName: 'Edwin',
-    lastName: 'Díaz',
-    position: 'RP',
-    age: 31,
-    photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/35394.png'
-  },
-  {
-    firstName: 'Ryne',
-    lastName: 'Stanek',
-    position: 'RP',
-    age: 33,
-    photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916468.png'
-  },
-  {
-    firstName: 'Reed',
-    lastName: 'Garrett',
-    position: 'RP',
-    age: 32,
-    photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916469.png'
-  },
-  {
-    firstName: 'José',
-    lastName: 'Buttó',
-    position: 'RP',
-    age: 27,
-    photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916470.png'
-  },
-  {
-    firstName: 'Génesis',
-    lastName: 'Cabrera',
-    position: 'RP',
-    age: 28,
-    photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/41061.png'
-  }
+
 ]
+
