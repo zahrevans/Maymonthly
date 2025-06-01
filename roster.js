ecd206fbb116ee45699d864985c896bf4a13b16b
zahrevans <526Zevans@frhsd.com>
2025-06-01 23:48:57 +0000

    back to top button

--- roster.js
+++ roster.js
@@ -119,3 +119,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 });
+
+// back to top button 
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#scroll').fadeIn();
+    } else {
+      $('#scroll').fadeOut();
+    }
+  });
+  $('#scroll').click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
+});
